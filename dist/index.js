56d73599141fcc9f5ecb002419a9637e3d943047
vikram <vikramshettyr4@gmail.com>
2025-08-05 09:57:43 +0530

    backend added

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // @ts-ignore
 const ws_1 = require("ws");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
+const wss = new ws_1.WebSocketServer({ port: 8070 });
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount = userCount + 1;
-    console.log("User connected #" + userCount);
     socket.on("message", (message) => {
-        console.log("message received " + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            // @ts-ignore
-            s.send(message.toString() + ":sent from the server");
+        // @ts-ignore: WebSocket message needs to be parsed
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                // @ts-ignore
+                room: parsedMessage.payload.roomId
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                // @ts-ignore
+                if (allSockets[i].socket === socket) {
+                    // @ts-ignore
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                // @ts-ignore
+                if (allSockets[i].room === currentUserRoom) {
+                    // @ts-ignore: Payload message is guaranteed to exist in chat messages
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,56 @@
   // @ts-ignore
-import { WebSocketServer, WebSocket } from "ws"
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8070 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+interface Message {
+    type: string;
+    payload: {
+        roomId?: string;
+        message?: string;
+    };
+}
+
+let allSockets: User[] = [];
+
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-
-    userCount = userCount + 1;
-    console.log("User connected #" + userCount)
 
     socket.on("message", (message) => {
-        console.log("message received " + message.toString());
-
-        for(let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            // @ts-ignore
-            s.send(message.toString() + ":sent from the server");
+        // @ts-ignore: WebSocket message needs to be parsed
+        const parsedMessage: Message = JSON.parse(message);
+        
+        if (parsedMessage.type === "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                  // @ts-ignore
+                room: parsedMessage.payload.roomId
+            });
         }
-    })
-})
+
+        if (parsedMessage.type === "chat") {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                  // @ts-ignore
+                if (allSockets[i].socket === socket) {
+                      // @ts-ignore
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                // @ts-ignore
+                if (allSockets[i].room === currentUserRoom) {
+                    // @ts-ignore: Payload message is guaranteed to exist in chat messages
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+    });
+});
